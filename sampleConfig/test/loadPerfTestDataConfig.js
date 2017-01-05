2c1ec7e320f6252bd9604be636753fe62fc1835c
Don Gaudino <don@panosoft.com>
2017-01-05 13:06:58 -0800

    Created.

---
+++ sampleConfig/test/loadPerfTestDataConfig.js
@@ -0,0 +1,13 @@
+var config = {
+	// optional parameter.  database connection timeout in milliseconds.  default value:  15000.
+	connectTimeout: 15000,
+	// connection parameters to event source database.  events generated in the loadPerfTestData test program will be inserted into the events table in this database.
+	eventSource: {
+		host: 'localhost',
+		databaseName: 'sourceDb',
+		user: 'user',
+		password: 'password'
+	}
+};
+
+module.exports = config;
